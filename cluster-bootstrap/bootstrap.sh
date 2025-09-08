d09b6eb944c785dd17806bf07dc6d392849211a8
Mohamed AbouDeif <M.AboDaif@outlook.com>
2025-09-09 02:17:00 +0300

    fix: update cluster bootstrap scrip

--- cluster-bootstrap/bootstrap.sh
+++ cluster-bootstrap/bootstrap.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+# Get directory of the script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-helm repo add argo https://argoproj.github.io/argo-helm
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
+# Change to that directory
+cd "$SCRIPT_DIR" || exit
 
-helm upgrade --install argocd argo/argo-cd -n argocd --create-namespace --wait --version 8.3.5
+# Check current directory
+pwd 
 
-helm upgrade --install prometheus-crds prometheus-community/prometheus-operator-crds \
-  -n monitoring --create-namespace --version 23.0.0 --wait
+# helm repo add argo https://argoproj.github.io/argo-helm
+# helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+# helm repo update
 
-kubectl apply -f ./manager-app.yaml # apply all needed base charts as argocd application
+# helm upgrade --install argocd argo/argo-cd -n argocd --create-namespace --wait --version 8.3.5
+
+# helm upgrade --install prometheus-crds prometheus-community/prometheus-operator-crds \
+#   -n monitoring --create-namespace --version 23.0.0 --wait
+
+# kubectl apply -f manager-app.yaml # apply all needed base charts as argocd application
